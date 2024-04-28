14ef4daa825b65471e1922114f7a7eb4a918daf2
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-04-28 02:02:34 -0300

    Add files Matrizes

---
+++ Matrizes/Matrizes/ExercicioFixacao.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+namespace Matrizes
+{
+    class ExercicioFixacao
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite os valores para m e n: ");
+            string[] matrix = Console.ReadLine().Split(' ');
+            int m = int.Parse(matrix[0]);
+            int n = int.Parse(matrix[1]);
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("---------------------------------");
+            Console.Write("Digite o valor que deseja procurar: ");
+            int v = int.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------------");
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == v)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[(i - 1), j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, (j + 1)]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[(i + 1), j]);
+                        }
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Matrizes/Matrizes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            //inicialização /instanciação de uma matriz
+            double[,] mat = new double[2, 3];
+
+            //propriedades
+
+            Console.WriteLine(mat.Length);      //mostra o total de elementos da matriz
+            Console.WriteLine(mat.Rank);        //mostra o primeiro termo da declaração da matriz (numero de linhas)
+            Console.WriteLine(mat.GetLength(0));//mostra a quantidade de linhas
+            Console.WriteLine(mat.GetLength(1));//mostra a quantidade de colunas
+        }
+    }
+}
