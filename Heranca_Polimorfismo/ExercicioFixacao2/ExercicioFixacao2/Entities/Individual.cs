c194ac6087e8f5fafede6e4b4420e40f8747f735
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-04-28 18:13:48 -0300

    Add files ExercicioFixacao2

---
+++ Heranca_Polimorfismo/ExercicioFixacao2/ExercicioFixacao2/Entities/Individual.cs
@@ -0,0 +1,24 @@
+﻿namespace ExercicioFixacao2.Entities
+{
+    internal class Individual : TaxPayer
+    {
+        public double HealthExpeditures { get; set; }
+
+        public Individual(string name, double anualIncome, double healthExpeditures) : base(name,anualIncome)
+        {
+            HealthExpeditures = healthExpeditures;
+        }
+
+        public override double Tax()
+        {
+            if (AnualIncome < 20000.0)
+            {
+                return AnualIncome * 0.15 - HealthExpeditures * 0.5;
+            }
+            else
+            {
+                return AnualIncome * 0.25 - HealthExpeditures * 0.5;
+            }
+        }
+    }
+}
