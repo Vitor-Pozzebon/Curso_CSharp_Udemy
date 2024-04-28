ec58bd5c3a27756168c2f5dddba038d1e6b3735b
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-04-28 02:04:15 -0300

    Add files Metodos Estaticos

---
+++ Metodos_Estaticos/MetodosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace MetodosEstaticos {
+    class ConversorDeMoeda {
+        public static double iof = 0.06;
+
+        public static double Calculadora(double v, double d) {
+            double valorFinal = v * d;
+
+            return valorFinal + (valorFinal * iof);
+        }
+    }
+}
---
+++ Metodos_Estaticos/MetodosEstaticos/Exercicio1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace MetodosEstaticos {
+    class Exercicio1 {
+        static void Main(string[] args) {
+            Console.Write("Qual a cotação do dólar?: ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar?: ");
+            double compra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.Calculadora(compra, dolar);
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
