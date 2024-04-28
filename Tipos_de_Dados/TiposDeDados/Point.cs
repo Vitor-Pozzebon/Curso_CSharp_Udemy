a24426c7b7606320b366739e29e7e04d2120a7a6
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-04-28 02:05:50 -0300

    Add files Tipos de dados

---
+++ Tipos_de_Dados/TiposDeDados/Point.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace TiposDeDados {
+    struct Point {
+        public double X;
+        public double Y;
+
+        public override string ToString() {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
---
+++ Tipos_de_Dados/TiposDeDados/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+
+namespace TipoDeDados {
+    class Program {
+        static void Main(string[] args) {
+            
+            Point p = new Point();
+            Console.WriteLine(p);
+
+             double? x = null;   //Nullable<double> x = null; é a mesma coisa
+             double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if(x.HasValue) {
+                Console.WriteLine(x.Value);
+            }
+            else {
+                Console.WriteLine("X is null");
+            }
+
+            if(y.HasValue) {
+                Console.WriteLine(y.Value);
+            }
+            else {
+                Console.WriteLine("Y is null");
+            }
+
+            Console.WriteLine("-------------------");
+
+            double? c = null;
+            double? v = 10;
+
+            double a = c ?? 5;
+            double b = v ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
