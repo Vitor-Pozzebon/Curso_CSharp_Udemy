c3f2a6db30800affbdc67e99422f4fa50bd0a423
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-04-28 01:59:59 -0300

    Add files Listas

---
+++ Listas/Listas/ListaExercicio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Listas {
+    class ListaExercicio {
+        static void Main(string[] args) {
+            
+            List<Funcionario> list = new List<Funcionario>();
+
+            Console.Write("Digite quantos funcionários serão registrados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("\nFuncionario #" + (i+1));
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Funcionario(id, name, salary));
+            }
+
+            Console.WriteLine("--------------------------------------");
+            foreach (Funcionario funcionario in list) {
+                Console.WriteLine(funcionario);
+            }
+
+            Console.Write("\nDigite o ID do funcionário que irá receber aumento de salário: ");
+            int idIncrease = int.Parse(Console.ReadLine());
+            
+            Funcionario f = list.Find(x => x.Id == idIncrease);
+
+            if(f != null) {
+                Console.Write("Entre com a porcentagem: ");
+                double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                f.IncreaseSalary(percent);
+            }
+            else {
+                Console.WriteLine("Esse ID não existe na lista!");
+            }
+            
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("\nDados atualizados dos funcionários: ");
+            foreach (Funcionario funcionario in list) {
+                Console.WriteLine(funcionario);
+            }
+        }
+    }
+}
---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;       //biblioteca para usar strings
+
+namespace Listas {
+    class Program {
+        static void Main(string[] args) {
+            
+            //padrão de instanciação de uma lista
+
+            //List<string> list = new List<string>();
+            //List<string> list2 = new List<string> { "Maria", "Alex" };
+
+            List<string> list = new List<string>();
+
+            //add - adiciona o elemento ao final da lista
+            //insert - adiciona o elemento em uma posição específica da lista
+            //count - mostra o tamanho da lista
+            //Find e FindLast - achar o primeiro ou último elemento da lista que satisfaça um predicado
+            //FindIndex e FindLastIndex - achar a primeira ou última posição da lista que satisfaça um predicado
+            //FindAll - filtra a lista toda com base em um predicado
+
+            //Remover elementos da lista:
+            //Remove - remove um elemento específico - deve passar o próprio elemento como parâmetro
+            //RemoveAll - remove um grupo de elementos de acordo com um predicado
+            //RemoveAt - remove um elemento específico baseado no índice da lista
+            //RemoveRange - remove uma faixa de elementos - precisa passar o índice inicial da remoção e a contagem de elementos que quer remover
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("Tamanho da lista: " + list.Count);
+            Console.WriteLine("---------------------------------------------------");
+
+            string s1 = list.Find(x => x[0] == 'A');        // x tal que x[0] == 'A'
+            Console.WriteLine("First 'A': " + s1);
+
+            string s2 = list.FindLast(x => x[0] ==  'A'); ;
+            Console.WriteLine("Last 'A': " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First index 'A': " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last index 'A': " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("---------------------------------------------------");
+            foreach (string obj in list2) {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveRange(2, 2);
+            Console.WriteLine("---------------------------------------------------");
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("---------------------------------------------------");
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
