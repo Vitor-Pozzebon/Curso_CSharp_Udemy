d294069a015df17139306ff567572a75aa4d0219
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-04-28 01:54:42 -0300

    Add files CadastroProdutos

---
+++ Cadastro_Produtos/CadastroProduto/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroProduto {
+    class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() {     //override diz que o método vem de outra classe
+            return Nome + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + ", " + Quantidade 
+                + " unidades, Total: $ " 
+                + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
