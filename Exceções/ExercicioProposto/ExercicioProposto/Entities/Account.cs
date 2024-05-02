454c99f71056afed7d9467fc065c62d06804a941
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-05-01 23:23:10 -0300

    Add files Exercício proposto Exceções

---
+++ Exceções/ExercicioProposto/ExercicioProposto/Entities/Account.cs
@@ -0,0 +1,41 @@
+﻿namespace ExercicioProposto.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public Account()
+        {
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount > Balance)
+            {
+                throw new Exceptions.DomainException("Not enough balance");
+            }
+            if (amount > WithdrawLimit)
+            {
+                throw new Exceptions.DomainException("The amount exceeds withdraw limit");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
