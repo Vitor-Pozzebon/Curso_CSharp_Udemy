ed63bc0c53892d87375eff6be7beb4d3f43833cb
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-11 20:40:59 -0300

    Add files via upload

---
+++ Arquivos/Pastas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringWriter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"D:\Projetos C#\Arquivos\myfolder";
+
+            try
+            {
+                //ver pastas
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //ver arquivos
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //criar pasta
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
