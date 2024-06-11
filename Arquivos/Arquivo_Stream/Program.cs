779d9592178ff2ff77ddc24e3933d15335595fb2
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-11 20:39:14 -0300

    Add files

---
+++ Arquivos/Arquivo_Stream/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos_Stream
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"D:\Projetos C#\Arquivos\file1.txt"; //@ é para não digitar duas barras
+
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);  //OpenText instancia o StreamReader e já le o arquivo
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }                 
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
