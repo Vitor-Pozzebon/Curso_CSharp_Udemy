794a28c250527d02287be1ea1c60564e781be069
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-11 20:41:43 -0300

    Add files via upload

---
+++ Arquivos/StringWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringWriter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sourcePath = @"D:\Projetos C#\Arquivos\file1.txt";
+            string targetPath = @"D:\Projetos C#\Arquivos\file2.txt";
+
+            try
+            {
+                //ler o conteudo do arquivo
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
