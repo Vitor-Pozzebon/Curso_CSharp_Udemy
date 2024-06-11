0c9a2bfe54fcd5bc5ae44858add50a16dd626c0d
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-11 20:40:05 -0300

    Add files via upload

---
+++ Arquivos/BlocoUsing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos_Stream
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"D:\Projetos C#\Arquivos\file1.txt"; //@ é para não digitar duas barras
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
