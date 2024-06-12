eca5a0471364386389799e4d3180fbe09d5a281f
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-12 19:02:34 -0300

    Add files via upload

---
+++ Interfaces/Interface_IComparable/Program.cs
@@ -0,0 +1,35 @@
+﻿using Interface_IComparable.Entities;
+using System;
+
+namespace Comparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Projetos C#\Interfaces\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp  in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
