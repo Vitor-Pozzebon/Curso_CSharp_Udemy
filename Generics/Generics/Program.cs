9349a95cfd9170f11a9108537e28acd4b1f42e4c
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-13 19:15:47 -0300

    Add files via upload

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using Generics;
+using System;
+
+namespace generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            //impressão do vetor
+            printService.Print();
+
+            //impressão do primeiro elemento do vetor
+            Console.WriteLine("\nFirst: " + printService.First());
+        }
+    }
+}
