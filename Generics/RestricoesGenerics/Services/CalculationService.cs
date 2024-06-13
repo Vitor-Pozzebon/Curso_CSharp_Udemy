5e39e131e77035d43d090b5a0e9f483ff22aafd1
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-13 19:16:12 -0300

    Add files via upload

---
+++ Generics/RestricoesGenerics/Services/CalculationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RestricoesGenerics.Services
+{
+    internal class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable     //para permitir fazer a comparação
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
