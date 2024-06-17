9b91ed11467285716f4cd70d6aa02e1504dfa2fd
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-17 19:54:43 -0300

    Add files via upload

---
+++ Generics/ExProposto_Dictionary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExPropostoDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(",");
+                        string candidate = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if(dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                        
+                    }
+
+                    foreach(var item in dictionary)
+                    {
+                      Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
