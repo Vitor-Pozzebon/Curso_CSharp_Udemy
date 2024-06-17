43916d46928fe8645f6b0236182861aa3339364d
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-17 19:55:07 -0300

    Add files via upload

---
+++ Generics/GetHashCode_Equals/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GetHashCode_Equals.Entities;
+
+namespace gethashcode_equals
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
