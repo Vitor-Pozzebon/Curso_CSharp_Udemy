6d9f8544534f4d2b2efb20ca83b8d534fdba03c2
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-17 19:53:17 -0300

    Add files via upload

---
+++ Generics/ColecoesHash/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ColecoesHash.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        //para verificar o hashcode
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        //para verificar os objetos com o Equals
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Product))
+            {
+                return false;
+            }
+
+            Product other = obj as Product;
+
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
