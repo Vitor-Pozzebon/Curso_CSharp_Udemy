7fc703751d5d37e875b149bfe4053c13d6bb74f0
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-20 19:06:02 -0300

    Add files via upload

---
+++ Lambda-Delegates-Linq/Predicate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Predicate.Entities;
+
+namespace Predicate
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //exemplo de predicate - delegando uma função
+            //predicate é um delegate
+            list.RemoveAll(ProductTest);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
