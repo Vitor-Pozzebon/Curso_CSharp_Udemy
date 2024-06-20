68086359e866886f1137e85887f834f3685ce696
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-20 19:04:14 -0300

    Add files via upload

---
+++ Lambda-Delegates-Linq/Action/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Action.Entities;
+
+namespace Action
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //exemplo de action - não retorna um valor (deve ser void)
+            //action é um delegate
+            list.ForEach(UpdatePrice);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
