5963c7166c6667f4833680aaf22f00eacde02dcc
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-20 19:05:42 -0300

    Add files via upload

---
+++ Lambda-Delegates-Linq/MulticastDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using MulticastDelegate.Services;
+using System;
+
+namespace MulticastDelegate
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            //para chamar as funções a serem executadas - ou op.Invoke(a, b)
+            op(a, b);
+        }
+    }
+}
