b8331fd1e3ffeb9536dc35bb1a44af7cf47544dc
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-20 19:05:15 -0300

    Add files via upload

---
+++ Lambda-Delegates-Linq/Func/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Func.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Action
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //exemplo de func - retorna um valor
+            //func é um delegate
+            List<string> result = list.Select(NameUpper).ToList();
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
