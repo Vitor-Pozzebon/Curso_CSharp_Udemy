bdd0066e80d7f1aff7ef81e440a5f6af8536e65a
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-24 18:12:34 -0300

    Add files via upload

---
+++ Lambda-Delegates-Linq/Linq-With-Lambda/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LinqWithLambda.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
