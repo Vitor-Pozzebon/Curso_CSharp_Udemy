3af24e2c3fb5344c65381227660cf94977bdde73
Vitor Pozzebon <87827192+Vitor-Pozzebon@users.noreply.github.com>
2024-06-24 18:12:08 -0300

    Add files via upload

---
+++ Lambda-Delegates-Linq/Introducao-Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Introducao_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // especificar o data source
+            int[] numbers = new int[] { 2,3,4,5 };
+
+            //definir a expressão de consulta
+            //x tal que x seja par e multiplicar esse número por 10
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // executar a consulta
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
